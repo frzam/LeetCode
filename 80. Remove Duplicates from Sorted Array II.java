593ae9fc86806a5d36642863c5f3de3be6824ba1
Farzam Alam <Farzamcse@gmail>
2019-08-21 17:46:52 +0530

    Day 23: 80

---
+++ 80. Remove Duplicates from Sorted Array II.java
@@ -0,0 +1,29 @@
+/**
+Given nums = [1,1,1,2,2,3],
+
+Your function should return length = 5, with the first five elements of nums being 1, 1, 2, 2 and 3 respectively.
+
+It doesn't matter what you leave beyond the returned length.
+Example 2:
+
+Given nums = [0,0,1,1,1,1,2,3,3],
+
+Your function should return length = 7, with the first seven elements of nums being modified to 0, 0, 1, 1, 2, 3 and 3 respectively.
+
+It doesn't matter what values are set beyond the returned length.
+
+Runtime: 0 ms, faster than 100.00% of Java online submissions for Remove Duplicates from Sorted Array II.
+Memory Usage: 37.3 MB, less than 100.00% of Java online submissions for Remove Duplicates from Sorted Array II.
+
+**/
+
+class Solution {
+ public int removeDuplicates(int[] nums) {
+    int i = 0;
+    for (int n : nums)
+        if (i < 2 || n > nums[i-2])
+            nums[i++] = n;
+    return i;
+}
+    
+    }
