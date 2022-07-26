0293ccda9b4405e4d10fd2da905714f1b27800f1
frzam <farzamcse@gmail.com>
2022-07-26 09:50:59 +0300

    blind-75: Q6 maximum sub array

---
+++ blind-75/max-subarr.go
@@ -0,0 +1,14 @@
+func maxSubArray(nums []int) int {
+	max, sum := nums[0], nums[0]
+	for i := 1; i < len(nums); i++ {
+		if sum < 0 {
+			sum = nums[i]
+		} else {
+			sum += nums[i]
+		}
+		if sum > max {
+			max = sum
+		}
+	}
+	return max
+}
